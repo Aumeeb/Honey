8616a1264314ec0de2215c969151f5d88b7a108a
noberk <421471260@qq.com>
2017-12-11 19:08:40 +0800

    using styled components instead of csstyle

--- src/routes/components/Footer/Footer.tsx
+++ src/routes/components/Footer/Footer.tsx
@@ -2,9 +2,8 @@
 import * as React from 'react'
 // import Link from '../Link/Link'
 import './Footer.css'
-import { Test} from './index'
 import code2d from '../../Images/code2d.jpg';
-
+import { ICP, PCopyRight,FooterWarpper,NavWrapper,NavSubWrapper } from './';
 interface IFooterProps {
     navData?: Array<{
         headerTitle?: string,
@@ -27,25 +26,25 @@ class Footer extends React.Component<IFooterProps, any> {
     render() {
         const { footerData } = this.props
 
+
         let footerDataExsits: string[];
-        if (footerData != null && footerData.length == 2) {
+        if (footerData !== undefined && footerData.length == 2) {
             footerDataExsits = footerData;
         } else {
-            footerDataExsits = ['无数据', '无数据'];
+            footerDataExsits = ['版权所有 @ 2000-2017 中软国际制造云 All Rights Reserved', '京ICP 备15012914号-12 京公网安备11001802022625'];
         }
         return (
             <div >
-                <Test>12312321</Test>
-                <address className='address'>
-                    <div>
+                <NavWrapper>
+                    <NavSubWrapper>
                         <header>新闻中心</header>
                         <nav>公司动态</nav>
                         <nav>媒体聚焦</nav>
                         <nav>社交媒体</nav>
                         <nav><img src={code2d} style={{ width: 'auto', height: 'auto', marginTop: 10 }} /></nav>
 
-                    </div>
-                    <div>
+                    </NavSubWrapper>
+                    <NavSubWrapper>
                         <header>投资者</header>
                         <nav>财务报表</nav>
                         <nav>联交所公告</nav>
@@ -54,8 +53,8 @@ class Footer extends React.Component<IFooterProps, any> {
                         <nav>财务摘要</nav>
                         <nav>股权结构</nav>
                         <nav>推介材料</nav>
-                    </div>
-                    <div>
+                    </NavSubWrapper>
+                    <NavSubWrapper>
                         <header>关于中软国际</header>
                         <nav>公司概览</nav>
                         <nav>愿景与使命</nav>
@@ -63,8 +62,8 @@ class Footer extends React.Component<IFooterProps, any> {
                         <nav>管理团队</nav>
                         <nav>公司荣誉</nav>
                         <nav>社会责任</nav>
-                    </div>
-                    <div>
+                    </NavSubWrapper>
+                    <NavSubWrapper>
                         <header>联系我们</header>
                         <nav>香港</nav>
                         <nav>北京</nav>
@@ -75,8 +74,8 @@ class Footer extends React.Component<IFooterProps, any> {
                         <nav>媒体关系</nav>
                         <nav>投资者关系</nav>
                         <nav>业务接洽</nav>
-                    </div>
-                    <div>
+                    </NavSubWrapper>
+                    <NavSubWrapper>
                         <header>加入我们</header>
                         <nav>CEO致辞</nav>
                         <nav>加入中软国际的10个理由</nav>
@@ -85,13 +84,13 @@ class Footer extends React.Component<IFooterProps, any> {
                         <nav>培训与晋升</nav>
                         <nav>快乐中软国际</nav>
                         <nav>工作机会</nav>
-                    </div>
-                </address>
+                    </NavSubWrapper>
+                </NavWrapper>
                 {/* 页脚 */}
-                <footer className='footer'>
-                    <p>{footerDataExsits[0]}</p>
-                    <p>{footerDataExsits[1]}</p>
-                </footer>
+                <FooterWarpper className='FooterWarpper'>
+                    <PCopyRight>{footerDataExsits[0]}</PCopyRight>
+                    <ICP>{footerDataExsits[1]}</ICP>
+                </FooterWarpper>
             </div>
         );
     }
--- src/routes/components/Footer/index.ts
+++ src/routes/components/Footer/index.ts
@@ -1,42 +1,49 @@
 import styled from 'styled-components';
 
 import bgn_downbar from '../../Images/bgn_downbar.jpeg';
-export const Address = styled.address`
+
+// 页脚的导航
+export const NavWrapper = styled.div`
     height: 375px;
     width: 100%;
     padding-left: 140px;
     padding-right: 100px;
     padding-top: 70px;
     margin-top: 30px;
-    background: url("../../Images/bgn_downbar.jpeg");
+    background: url(${bgn_downbar});
+`;
+export const NavSubWrapper = styled.div`
+float: left;
+height: 100%;
+width: 20%;
+color: white;
 `;
 
-export const Test = styled.div``
+export const FooterWarpper = styled.footer`
+font-size: 12px;
+color:white;
+height: 76px;
+text-align: center;
+background: -webkit-linear-gradient(left, #03bce0 , #2783ce); /* Safari 5.1 - 6.0 */
+background: -o-linear-gradient(left, #03bce0 , #2783ce); /* Opera 11.1 - 12.0 */
+background: -moz-linear-gradient(left,  #03bce0 , #2783ce); /* Firefox 3.6 - 15 */
+background: linear-gradient(to left,  #03bce0 , #2783ce); /* 标准的语法 */
+`
 
-// .container {
-//     margin: 0 auto;
-//     padding: 20px 15px;
-//     max-width: var(--max-content-width);
-//     text-align: center;
-//   }
+const P = styled.p`
+   margin:0;
+`;
+
+export const PCopyRight =styled(P)`
+padding-top: 20px;
+`
+export const ICP  =styled(P)`
+padding-top: 10px;
+`
+ 
 
 
-
-//   .address{
-//     height: 375px;
-//     width: 100%;
-//     padding-left: 140px;
-//     padding-right: 100px;
-//     padding-top: 70px;
-//     margin-top: 30px;
-//     background: url("../../../public/bgn_downbar.jpeg");
-//   }
-//   .address >div{
-//     float: left;
-//     height: 100%;
-//     width: 20%;
-//     color: white;
-//   }
+ 
 //   .address > div>header:first-child{
 
 //     height: 40px;
@@ -75,19 +82,3 @@ export const Test = styled.div``
 
 //   }
 
-//   .footer{
-//     font-size: 12px;
-//     color:white;
-//     height: 76px;
-//     text-align: center;
-//     background: -webkit-linear-gradient(left, #03bce0 , #2783ce); /* Safari 5.1 - 6.0 */
-//   background: -o-linear-gradient(left, #03bce0 , #2783ce); /* Opera 11.1 - 12.0 */
-//   background: -moz-linear-gradient(left,  #03bce0 , #2783ce); /* Firefox 3.6 - 15 */
-//   background: linear-gradient(to left,  #03bce0 , #2783ce); /* 标准的语法 */
-//   }
-//   .footer > p:first-child{
-//     padding-top: 20px;
-//   }
-//   .footer > p:last-child{
-//     padding-top: 10px;
-//   }
